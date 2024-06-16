00ba313fe8a94d6d28d2818795154f8e565434fa
y0ng98 <dldydgnl2080@gmail.com>
2024-06-16 23:07:25 +0900

    feat: [이용휘] 게시글 리스트 출력 가능 [PROJ-DJANGO-5]

---
+++ emoticon_seller/board/entity/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Board(models.Model):
+    boardId = models.AutoField(primary_key=True)
+    boardTitle = models.CharField(max_length=128, null=False)
+    boardWriter = models.CharField(max_length=32, null=False)
+    boardContent = models.TextField()
+    boardImage = models.CharField(max_length=100, null=True)
+    boardRegDate = models.DateTimeField(auto_now_add=True)
+    boardUpdDate = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.boardTitle
+
+    class Meta:
+        db_table = 'doc_board'
