e7945e0b46adc2a49ed55ad6bcc00ae3b1d9ae6f
Minji Choi <133327408+Minn-ji@users.noreply.github.com>
2024-06-17 15:45:17 +0900

    feat: [최민지] 상품 리스트 출력 [PROJ-DJANGO-1]

---
+++ emoticon_seller/product/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from product.entity.models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['productId', 'productName', 'productPrice', 'productImage', 'writer', 'content', 'regDate', 'updDate']
+        read_only_fields = ['regDate', 'updDate']
