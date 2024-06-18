2b68e22b9b0cfc2212192dd3aed6335e789f23ad
lyw00 <taengo69@naver.com>
2024-06-18 23:31:53 +0900

    feat: [이용우] Product에서 상품 누르면 정보 출력&뒤로가기,구매버튼 연동[PROJ-DJANGO-8]
    feat: [이용우] Product에서 상품 누르면 정보 출력&뒤로가기,구매버튼  연동[PROJ-DJANGO-8]

--- emoticon_seller/product/repository/product_repository.py
+++ emoticon_seller/product/repository/product_repository.py
@@ -9,3 +9,7 @@ class ProductRepository(ABC):
     @abstractmethod
     def create(self, productName, productPrice, writer, content, productImage):
         pass
+
+    @abstractmethod
+    def findByProductId(self, productId):
+        pass
