4017d808b76ed79152f1c85a9c68c6a3c43652c7
Minji Choi <133327408+Minn-ji@users.noreply.github.com>
2024-06-26 11:59:29 +0900

    chore: [최민지] impl에서 구현 안된 cartList 주석처리
    코드가 안 돌아가서 일단 주석처리 합니다.

--- emoticon_seller/cart/service/cart_service.py
+++ emoticon_seller/cart/service/cart_service.py
@@ -5,7 +5,7 @@ class CartService(ABC):
     @abstractmethod
     def cartRegister(self, cartData, accountId):
         pass
-
-    @abstractmethod
-    def cartList(self, accountId):
-        pass
+    #
+    # @abstractmethod
+    # def cartList(self, accountId):
+    #     pass
