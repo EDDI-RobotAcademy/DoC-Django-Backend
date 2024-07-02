8568dabc349d5266273806837cdd012b9d732085
Minji Choi <133327408+Minn-ji@users.noreply.github.com>
2024-07-02 23:27:16 +0900

    fix: [최민지] report 컬럼 이름 수정

--- emoticon_seller/report/entity/report.py
+++ emoticon_seller/report/entity/report.py
@@ -3,13 +3,13 @@ from account.entity.account import Account
 
 
 class Report(models.Model):
-    reportId = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='reports')
     age = models.PositiveIntegerField()
     gender = models.CharField(max_length=3)
 
     def __str__(self):
-        return f"Report -> id: {self.reportId}, account: {self.account.id}"
+        return f"Report -> id: {self.id}, account: {self.account.id}"
 
     class Meta:
         db_table = 'report'
